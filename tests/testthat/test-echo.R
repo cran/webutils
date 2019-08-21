02ff4d02c0b871dc3c867c90815456e09d13f359
Jeroen Ooms <jeroen@berkeley.edu>
2019-08-21 11:40:06 +0000

    version 1.0

--- tests/testthat/test-echo.R
+++ tests/testthat/test-echo.R
@@ -4,7 +4,7 @@ context("echo with httpuv")
 test_that("test echo from httpuv", {
   desc <- system.file("DESCRIPTION")
   logo <- file.path(Sys.getenv("R_DOC_DIR"), "html/logo.jpg")
-  h <- curl::handle_setform(curl::new_handle(),
+  h <- curl::handle_setform(curl::new_handle(forbid_reuse = TRUE),
      foo = "blabla",
      bar = charToRaw("boeboe"),
      iris = curl::form_data(serialize(iris, NULL), "application/rda"),
@@ -41,13 +41,17 @@ test_that("Echo a big file", {
   # Create a random file (~30 MB)
   # Note: can test even bigger files but curl_echo() is a bit slow on Windows
   tmp <- tempfile()
-  n <- runif(1, 3e6, 4e6)
-  buf <- serialize(rnorm(n), NULL)
+  size <- if(.Platform$OS.type == "windows"){
+    1e5
+  } else {
+    runif(1, 3e6, 4e6)
+  }
+  buf <- serialize(rnorm(size), NULL)
   writeBin(buf, tmp)
   on.exit(unlink(tmp))
 
   # Roundtrip via httpuv
-  h <- curl::handle_setform(curl::new_handle(), myfile = curl::form_file(tmp))
+  h <- curl::handle_setform(curl::new_handle(forbid_reuse = TRUE), myfile = curl::form_file(tmp))
   req <- curl::curl_echo(h)
   formdata <- parse_http(req$body, req$content_type)
 
